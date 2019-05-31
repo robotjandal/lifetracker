03b5bf6467bf9e8425a837a9511d7b2ba54655cd
Dilpesh Patel <robotjandal@gmail.com>
2019-05-31 20:24:39 +1200

    Introducing the ability to create, update and delete goals based on the flaskr tutorial app.

--- lifetracker/goals.py
+++ lifetracker/goals.py
@@ -1,10 +1,48 @@
 from flask import (
     Blueprint,
+    flash,
+    g,
+    redirect,
     render_template,
+    request,
+    url_for,
 )
+from werkzeug.exceptions import abort
+
+from lifetracker.auth import login_required
 from lifetracker.db import get_db
 
 bp = Blueprint("goals", __name__)
+
+
+def get_goal(id, check_author=True):
+    """
+        Get a goal by id and author.
+
+        :param id: id of goal to get
+        :param check_author: require the current user to be the author
+        :return: the goal with author information
+        :raise 404: if a goal with the given id doesn't exist
+        :raise 403: if the current user isn't the author
+    """
+    goal = (
+        get_db()
+        .execute(
+            "SELECT g.id, title, created, author_id, username"
+            " FROM goals g JOIN user u ON g.author_id = u.id"
+            " WHERE g.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if goal is None:
+        abort(404, "Gost {0} doesn't exist.".format(id))
+
+    if check_author and goal["author_id"] != g.user["id"]:
+        abort(403)
+
+    return goal
 
 
 @bp.route("/")
@@ -12,8 +50,73 @@ def index():
     """ Current goals are displayed."""
     db = get_db()
     goals = db.execute(
-        "SELECT p.id, title, created, author_id, username"
-        " FROM goals p JOIN user u ON p.author_id = u.id"
+        "SELECT g.id, title, created, author_id, username"
+        " FROM goals g JOIN user u ON g.author_id = u.id"
         " ORDER BY created DESC"
     ).fetchall()
     return render_template("goals/index.html", goals=goals)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    """
+        Create a new goal for the current user.
+    """
+    if request.method == "POST":
+        title = request.form["title"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO goals (title, author_id)" " VALUES (?, ?)",
+                (title, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("goals.index"))
+
+    return render_template("goals/create.html")
+
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@login_required
+def update(id):
+    """Update a goal if the current user is the author."""
+    goal = get_goal(id)
+    if request.method == "POST":
+        title = request.form["title"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute("UPDATE goals SET title = ? WHERE id = ?", (title, id))
+            db.commit()
+            return redirect(url_for("goals.index"))
+
+    return render_template("goals/update.html", goal=goal)
+
+
+@bp.route("/<int:id>/delete", methods=("POST",))
+@login_required
+def delete(id):
+    """Delete a goal.
+    Ensures that the goal exists and that the logged in user is the
+    author of the goal.
+    """
+    get_goal(id)
+    db = get_db()
+    db.execute("DELETE FROM goals WHERE id = ?", (id,))
+    db.commit()
+
+    return redirect(url_for("goals.index"))
--- tests/test_goals.py
+++ tests/test_goals.py
@@ -1,3 +1,5 @@
+import pytest
+from lifetracker.db import get_db
 
 
 def test_index(client, auth):
@@ -8,8 +10,52 @@ def test_index(client, auth):
     auth.login()
     response = client.get("/")
     print(response.data)
-    # Commented out until new data can be captured (next commit)
-    # assert b"test title" in response.data
-    # assert b"by test on 2018-01-01" in response.data
-    # assert b"test\nbody" in response.data
-    # assert b'href="/1/update"' in response.data
+    assert b"test title" in response.data
+    assert b"by test on 2018-01-01" in response.data
+    assert b'href="/1/update"' in response.data
+
+
+@pytest.mark.parametrize("path", ("/2/update", "/2/delete"))
+def test_exists_required(client, auth, path):
+    auth.login()
+    assert client.post(path).status_code == 404
+
+
+def test_create(client, auth, app):
+    auth.login()
+    assert client.get("/create").status_code == 200
+    client.post("/create", data={"title": "created", })
+
+    with app.app_context():
+        db = get_db()
+        count = db.execute("SELECT COUNT(id) FROM goals ").fetchone()[0]
+        assert count == 2
+
+
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get("/1/update").status_code == 200
+    client.post("/1/update", data={"title": "updated", })
+
+    with app.app_context():
+        db = get_db()
+        goal = db.execute("SELECT * FROM goals WHERE id = 1").fetchone()
+        assert goal["title"] == "updated"
+
+
+@pytest.mark.parametrize("path", ("/create", "/1/update"))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={"title": "", })
+    assert b"Title is required." in response.data
+
+
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post("/1/delete")
+    assert response.headers["Location"] == "http://localhost/"
+
+    with app.app_context():
+        db = get_db()
+        goal = db.execute("SELECT * FROM goals WHERE id = 1").fetchone()
+    assert goal is None
